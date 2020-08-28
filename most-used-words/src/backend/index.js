1452fb56e50058421a9cc8bc6844fdb8e80ca12a
Juan Pablo <juanpablo192.dreaw@gmail.com>
2020-08-28 11:04:19 -0300

    adding path to rows and file test

--- most-used-words/src/backend/index.js
+++ most-used-words/src/backend/index.js
@@ -1,14 +1,18 @@
 const { ipcMain } = require("electron");
 
-ipcMain.on("process-subtitles", (event, paths) => {
-    console.log(paths);
+const pathsToRows = require("./pathsToRow");
 
-    event.reply("process-subtitles", [
-        { name: "i", amount: 1234 },
-        { name: "you", amount: 900 },
-        { name: "he", amount: 345 },
-        { name: "she", amount: 345 },
-        { name: "hi", amount: 978 },
-        { name: "bina", amount: 18 }
-    ]);
+ipcMain.on("process-subtitles", (event, paths) => {
+    pathsToRows(paths)
+        .then(rows => console.log(rows))
+        .then(() => {
+            event.reply("process-subtitles", [
+                { name: "i", amount: 1234 },
+                { name: "you", amount: 900 },
+                { name: "he", amount: 345 },
+                { name: "she", amount: 345 },
+                { name: "hi", amount: 978 },
+                { name: "bina", amount: 18 }
+            ]);
+        });
 });
