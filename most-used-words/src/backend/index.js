e72f82ffc6fbf935dd24a54942ea61ffcc0cd68d
Juan Pablo <juanpablo192.dreaw@gmail.com>
2020-08-31 21:30:45 -0300

    import to index

--- most-used-words/src/backend/index.js
+++ most-used-words/src/backend/index.js
@@ -2,19 +2,11 @@ const { ipcMain } = require("electron");
 
 const pathsToRows = require("./pathsToRow");
 const preparaData = require("./preparaData");
+const groupWords = require("./groupWords");
 
 ipcMain.on("process-subtitles", (event, paths) => {
     pathsToRows(paths)
         .then(rows => preparaData(rows))
-        .then(words => console.log(words))
-        .then(() => {
-            event.reply("process-subtitles", [
-                { name: "i", amount: 1234 },
-                { name: "you", amount: 900 },
-                { name: "he", amount: 345 },
-                { name: "she", amount: 345 },
-                { name: "hi", amount: 978 },
-                { name: "bina", amount: 18 }
-            ]);
-        });
+        .then(words => groupWords(words))
+        .then(groupedWords => event.reply("process-subtitles", groupedWords));
 });
